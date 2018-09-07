bd6c2e8a9d78123fbecd5a4e8c633366580ac702
zhuk <gukolka@inbox.ru>
2018-09-07 12:37:47 +0300

    Lesson 3

--- src/by/it/zhuk/lesson02/TaskC3.java
+++ src/by/it/zhuk/lesson02/TaskC3.java
@@ -1,5 +1,8 @@
 package by.it.zhuk.lesson02;
 
+
+import java.util.Scanner;
+
 /*
 Ускорение свободного падения на Земле и Марсе таково:
 Марс   3.86
@@ -31,4 +34,20 @@ package by.it.zhuk.lesson02;
 */
 class TaskC3 {
 
+    static double getWeight(int weight) {
+        double result = weight * 3.86 / 9.81;
+        double r100 = result * 100;
+        double delta = r100 - (int) r100;
+        if (delta < 0.5)
+            return (int) r100 / 100.0;
+        else
+            return ((int) r100 + 1) / 100.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        System.out.println(getWeight(m));
+
+    }
 }
